cc2ca8fbec1641432d54770eacec9b8e5d329ef6
Polewique <maeva.bachelard.savoie@gmail.com>
2026-04-12 15:09:07 +0200

    feat: Add Reader class and read idx

--- src/reader.py
+++ src/reader.py
@@ -1,1 +1,12 @@
-print("hello")
+import pandas as pd
+
+class Reader:
+    def __init__(self, database_path):
+        self.database_path = database_path
+        self.database = pd.read_csv(database_path)
+
+    def read(self, idx=None):
+        if idx is not None:
+            return self.database.iloc[idx]
+        return self.database
+    
