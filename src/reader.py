c6e1f991261616f5e383d4557ee4fedccad72e0d
Victor <victor.xu1239@gmail.com>
2026-04-26 15:37:16 +0200

    feature : ruff checked

--- src/reader.py
+++ src/reader.py
@@ -1,8 +1,4 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from scipy import stats
-import os
 
 class Reader:
     def __init__(self, database_path):
